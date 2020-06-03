2cf223b5811915ab22f774a38297f1aed1de9f15
Rafa Moreno <rmh1516@gmail.com>
2020-06-03 14:28:59 +0200

    add desktop styles and main components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="container">
+        <div className="actions">
+          <img src="/images/logo.svg" alt="logo" />
+          <div>
+            <div className="icon">
+              <img src="/images/icon-document.svg" alt="document" />
+            </div>
+            <div className="icon">
+              <img src="/images/icon-folder.svg" alt="folder" />
+            </div>
+            <div className="icon">
+              <img src="/images/icon-upload.svg" alt="upload" />
+            </div>
+          </div>
+        </div>
+        <div className="storage">
+          <p>
+            You've used <span>815 GB</span> of your storage
+          </p>
+          <div>
+            <div className="bar">
+              <div className="used">
+                <div className="oval" />
+              </div>
+            </div>
+            <div className="minmax">
+              <span>0 GB</span>
+              <span>1000 GB</span>
+            </div>
+          </div>
+          <div className="left">
+            <p>
+              <span>185</span>GB LEFT
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
